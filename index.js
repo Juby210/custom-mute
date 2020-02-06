518df997c53f29cf8b8a4534f49a71b72aae8ab2
Juby210 <juby210@vp.pl>
2020-02-06 16:40:43 +0100

    memes

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 const { Plugin } = require('powercord/entities')
-const { getModule, i18n: { Messages }, React } = require('powercord/webpack')
+const { getModule, getModuleByDisplayName, i18n: { Messages }, React } = require('powercord/webpack')
 const { inject, uninject } = require('powercord/injector')
 const { ContextMenu: { Button, ItemGroup, Slider } } = require('powercord/components')
 
-const getDefultByDisplayName = name => getModule(m => m.default && m.default.displayName == name, true, true)
-
 module.exports = class CustomMute extends Plugin {
     async startPlugin() {
-        const mod = await getModule(['updateNotificationSettings'])
-        const ChannelTimedMuteGroup = await getDefultByDisplayName('ChannelTimedMuteGroup')
+        const SubMenuItem = await getModuleByDisplayName('FluxContainer(SubMenuItem)')
 
-        inject('cmchannel', ChannelTimedMuteGroup, 'default', (args, res) => {
-            let h = 0, m = 0
-            res.props.children = [ res.props.children, React.createElement(ItemGroup, { children: [
-                React.createElement(Slider, { name: 'Hours', initialValue: 0, onValueChange: val => h = Math.round(val) }),
-                React.createElement(Slider, { name: 'Minutes', initialValue: 0, onValueChange: val => m = Math.round(val) }),
-                React.createElement(Button, { name: 'Apply', onClick: () => {
-                    if (!h && !m) return
-                    mod.updateChannelOverrideSettings(args[0].channel.guild_id, args[0].channel.id, this.getMuteConfig(h, m))
-                }})
-            ]})]
+        inject('cmguild', SubMenuItem.prototype, 'render', (_, res) => {
+            if (res.props.label != Messages['MUTE_SERVER']) return res
+            const { render } = res.props
+            res.props.render = () => {
+                const r = render()
+                if (!r.props.guild) return r
 
-            return res
-        })
-        ChannelTimedMuteGroup.default.displayName = 'ChannelTimedMuteGroup'
-
-        const GuildContextMenu = await getDefultByDisplayName('GuildContextMenu')
-
-        inject('cmguild', GuildContextMenu, 'default', (args, res) => {
-            const muteGroup = res.props.children.find(({ props: { children: r } }) =>
-                r && r.props && r.props.label == Messages['MUTE_SERVER'])
-            if (!muteGroup) return res
-            const { render } = muteGroup.props.children.props
-            muteGroup.props.children.props.render = () => {
-                let h = 0, m = 0
-                return [ render(), React.createElement(ItemGroup, { children: [
-                    React.createElement(Slider, { name: 'Hours', initialValue: 0, onValueChange: val => h = Math.round(val) }),
-                    React.createElement(Slider, { name: 'Minutes', initialValue: 0, onValueChange: val => m = Math.round(val) }),
-                    React.createElement(Button, { name: 'Apply', onClick: () => {
-                        if (!h && !m) return
-                        mod.updateNotificationSettings(args[0].guild.id, this.getMuteConfig(h, m))
-                    }})
-                ]})]
+                return [ r, this.customMuteGroup(r.props.guild.id) ]
             }
 
             return res
         })
-        GuildContextMenu.default.displayName = 'GuildContextMenu'
+
+        const ChannelTimedMuteGroup = await getModule(m => m.default && m.default.displayName == 'ChannelTimedMuteGroup')
+
+        inject('cmchannel', ChannelTimedMuteGroup, 'default', (args, res) => {
+            res.props.children = [ res.props.children,
+                this.customMuteGroup(args[0].channel.guild_id, args[0].channel.id) ]
+
+            return res
+        })
+        ChannelTimedMuteGroup.default.displayName = 'ChannelTimedMuteGroup'
     }
 
     pluginWillUnload() {
-        uninject('cmchannel')
         uninject('cmguild')
+        uninject('cmchannel')
     }
 
+    customMuteGroup(gid, id) {
+        let h = 0, m = 0
+        return React.createElement(ItemGroup, { children: [
+            React.createElement(Slider, { name: 'Hours', initialValue: 0, onValueChange: val => h = Math.round(val) }),
+            React.createElement(Slider, { name: 'Minutes', initialValue: 0, onValueChange: val => m = Math.round(val) }),
+            React.createElement(Button, { name: 'Apply',
+                onClick: async () => {
+                    if (!h && !m) return
+
+                    const mod = await getModule(['updateNotificationSettings'])
+                    if (id) return mod.updateChannelOverrideSettings(gid, id, this.getMuteConfig(h, m))
+                    mod.updateNotificationSettings(gid, this.getMuteConfig(h, m))
+                }
+            })
+        ]})
+    }
     getMuteConfig(h, m) {
         const s = h * 3600 + m * 60
         return { muted: true, mute_config: {
